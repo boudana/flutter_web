be8af8a739c9169299e13c84d8b65a71fea06b9f
abdallah boudana <dzmysterious@gmail.com>
2020-06-06 21:31:50 +0100

    add main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/ui/shared/nav_bar.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -13,35 +15,49 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatelessWidget {
   static const routeName = 'homePage';
   @override
   Widget build(BuildContext context) {
-    
-    return Container(child: Column (
-     children: [
+    return Scaffold(
+      body: Container(
+          child: Column(
+            children: [
       NavBarWidget(),
       HomeContent(),
-     ],
-    ),
-      
+            ],
+          ),
+        ),
     );
   }
 }
-class NavBarWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      color: Colors.cyan,
-    );
-  }
-}
+
+
+
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+     
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Flexible(child: Container( child: Text('mohees4564564564dgafasfasfasfasfas safasfas fasfasfasfasfasfasfsafsaf asfasffasfafasf'))),
+              Flexible(
+                              child: Container(
+                  height: 500,
+                  width: 600,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/social-m-m.jpg'))),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
